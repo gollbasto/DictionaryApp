29e2f969ab310bcf7eb51085a805729cc47c382c
hanashi <EVShapin.SBT@sberbank.ru>
2017-07-25 14:17:30 +0300

    ошибка

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -8,7 +8,7 @@ import storage.AbbrCollection;
 
 public class Main extends Application
 {
-   private static final String COLLECTION_PATH = "./res/abbr.txt";
+   public static final String COLLECTION_PATH = "./res/abbr.txt";
    private static final String APP_ICON_PATH =  "file:res/app.png";
    private static final String APP_TITLE = "AbbrDictionaryApp";
    public static UserScreen screen = new UserScreen();
